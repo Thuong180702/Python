7a7460aa63d0bd8e00bb47d367add67e060cd15a
Thuong180702 <vanthuong180702t@gmail.com>
2023-10-06 10:51:19 +0700

    New

--- Function/Map/BT4.py
+++ Function/Map/BT4.py
@@ -1,5 +1,9 @@
 n = int(input("Nhập số: "))
-l = [i for i in range(2,n)]
-a=0
-print(list(map(lambda i: all(n%i!=0) ,l)))
+l = [i for i in range(2, n)]
+a = 0
+List_nt =list(map(lambda i:1 if n % i != 0 else 0, l))
 
+if 0 in List_nt:
+    print("Kphai")
+else:
+    print("Phai")
